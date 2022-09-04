81cf2a4f114691ec3908a196ba2b9760ae123964
hancoccc <98192793+hancoccc@users.noreply.github.com>
2022-09-04 14:50:10 -0400

    Create prime_factorization.py

---
+++ prime_factorization.py
@@ -0,0 +1,19 @@
+def prime_factorization(n):
+    x = n
+    d =2
+    numList = []
+    while(d<n):
+        while (x%d==0):
+            numList.append(d)
+            x = x/d
+        d = d+1
+    if (len(numList) == 0):
+        numList.append(n)
+    return(numList)
+        
+def main():
+    n = int(input('> '))
+    print(prime_factorization(n))
+
+if __name__ == "__main__":
+    main()
