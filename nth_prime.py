ae3ac18c54108519510b82cccabce9b52ad1e358
hancoccc <98192793+hancoccc@users.noreply.github.com>
2022-09-04 15:43:37 -0400

    Update nth_prime.py

--- nth_prime.py
+++ nth_prime.py
@@ -1,17 +1,29 @@
-def nth_prime(n):
-    count = 1
-    x=n
-    d =2
-    while(d<n):
-        if (x%d==0):
-    	    dict[count] = d
-    	    count = count+1
-    	    x=x/d
-        d = d+1
-        
+def nth_prime(n, primeList):
+    d=1
+    x=3
+    while (d<n):
+        tracker=0
+        for item in primeList:
+            if x%item==0:
+                tracker=1
+                break
+            else:
+                continue
+        if tracker==0:
+            primeList.append(x)
+            d=d+1
+            x=x+2
+        else:
+            x=x+2
+    return primeList
+    
 def main():
     n = int(input('> '))
-    print(nth_prime(n))
+    primeList=[2]
+    print(nth_prime(n, primeList))
 
 if __name__ == "__main__":
     main()
+    
+    
+    
